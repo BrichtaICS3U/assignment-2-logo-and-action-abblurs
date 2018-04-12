9fe36e5181cffa871e87a5e141860e57a8056e77
Abbey-J <ajayn1@ocdsb.ca>
2018-04-12 11:04:45 -0400

    First commit, Logo almost done

--- logo.py
+++ logo.py
@@ -1,6 +1,6 @@
 # ICS3U
 # Assignment 2: Logo
-# <your name>
+# Abbey Jayne
 
 # adapted from http://www.101computing.net/getting-started-with-pygame/
 
@@ -10,10 +10,11 @@ pygame.init()
 
 # Define some colours
 # Colours are defined using RGB values
-BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+BACKGROUNDGREEN = (99, 184, 86)
+DARKGREEN = (50, 140, 38)
+BEIGE = (242, 203, 46)
+BROWN = (138, 118, 40)
 
 # Set the screen size (please don't change this)
 SCREENWIDTH = 400
@@ -23,7 +24,7 @@ SCREENHEIGHT = 400
 # The window is defined as (width, height), measured in pixels
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Logo")
+pygame.display.set_caption("Abbey's Google Classroom Logo")
 
 # This loop will continue until the user exits the game
 carryOn = True
@@ -47,10 +48,25 @@ while carryOn:
     screen.fill(WHITE)
 
     # Queue different shapes and lines to be drawn
-    # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
-    # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
-
+    pygame.draw.rect(screen, BEIGE, [35, 30, 330, 250], 0) #backgrounds
+    pygame.draw.rect(screen, BACKGROUNDGREEN, [55, 50, 290, 210], 0)
+    pygame.draw.ellipse(screen, DARKGREEN, [95, 120, 40, 40], 0) #student heads
+    pygame.draw.ellipse(screen, DARKGREEN, [265, 120, 40, 40], 0)
+    pygame.draw.rect(screen, DARKGREEN, [80, 185, 70, 20], 0) #student bodies
+    pygame.draw.ellipse(screen, DARKGREEN, [80, 175, 70, 20], 0)
+    pygame.draw.rect(screen, DARKGREEN, [250, 185, 70, 20], 0)
+    pygame.draw.ellipse(screen, DARKGREEN, [250, 175, 70, 20], 0)
+    pygame.draw.ellipse(screen, WHITE, [170, 90, 60, 60], 0) #teacher head
+    pygame.draw.rect(screen, WHITE, [125, 175, 150, 30], 0) #teacher body
+    pygame.draw.ellipse(screen, WHITE, [125, 160, 150, 30], 0)
+    pygame.draw.rect(screen, WHITE, [225, 247, 70, 13], 0) #chalk
+    pygame.draw.polygon(screen, BROWN, [[225, 260], [240, 260],[240, 279]], 0) #chalk shadow
+    pygame.draw.polygon(screen, BROWN, [[295, 260], [295, 279],[310, 279]], 0)
+    pygame.draw.rect(screen, BROWN, [240, 260, 55, 20], 0)
+    font = pygame.font.SysFont("Calibri", 90, True)   #Text
+    text = font.render("Classroom", 3, (115, 115, 115))
+    screen.blit(text, (10, 300))
+    
     # Update the screen with queued shapes
     pygame.display.flip()
 
